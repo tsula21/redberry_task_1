53cbe1f3e13ec29b6a95a79ca4940903198e9385
tsula21 <ika.tsulaia21@mail.ru>
2023-02-06 16:37:05 +0400

    modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Modal.css";
+//
+import close from "../../Assets/form/close.svg";
 
 const Modal = () => {
-  return <div>Modal</div>;
+  return (
+    <div className="done_modal">
+      <div className="done_modal_container">
+        <img src={close} alt="close" />
+        <p>რეზიუმე წარმატებით გაიგზავნა 🎉</p>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
